cc57e8307cca1f962a75ab23b1cb12b5cd81ff7a
Zohaib ALi <Zali11@student.gsu.edu>
2021-03-10 10:36:23 -0500

    contact list

---
+++ contacts list.py
@@ -0,0 +1,11 @@
+import random
+
+class Contact:
+    def __init__(self, name, email, phone, address):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+print("_", end=" ")
+        print("")
+
